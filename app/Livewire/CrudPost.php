0df5e80924b72527469a4ff5325bf54217a663ab
Franchesto <frandreotti10@gmail.com>
2025-03-11 11:40:40 -0300

    gre

--- app/Livewire/CrudPost.php
+++ app/Livewire/CrudPost.php
@@ -11,7 +11,7 @@ use Livewire\Attributes\Computed;
 use Livewire\Component;
 use Livewire\WithPagination;
 
-class PostManager extends Component
+class CrudPost extends Component
 {
     use WithPagination;
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(PostInterface::class, PostService::class);
     }
 
     /**
